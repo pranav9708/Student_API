aabf13c9cc2cd09fe527734c8e3a368f2a7ab267
Pranav V <pranav9708@gmail.com>
2023-05-05 14:13:51 +0530

    added proper status codes

--- controller/student_controller.js
+++ controller/student_controller.js
@@ -1,71 +1,67 @@
-const Student=require('../models/student');
+const Student = require('../models/student');
 
-module.exports.createStudent=async (req,res)=>{
-    try{
-        const user=await Student.create(req.body);
-        if(user){
-            return res.send(user);
-        }else{
-            return res.send("Student not created")
-        }
-    }catch(err){
-        console.log("Error",err);
-        return res.status(500);
+module.exports.createStudent = async (req, res) => {
+  try {
+    const user = await Student.create(req.body);
+    if (user) {
+      return res.status(201).json(user);
+    } else {
+      return res.status(400).json({ message: "Student not created" });
     }
+  } catch (err) {
+    console.log("Error", err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-module.exports.getAll=async (req,res)=>{
-    try{
-        const user=await Student.find({});
-        if(user){
-            return res.send(user);
-        }else{
-            return res.send("Student Not found");
-        }
-    }catch(err){
-        console.log("Error",err);
-        return res.status(500);
-    }
+module.exports.getAll = async (req, res) => {
+  try {
+    const users = await Student.find({});
+    return res.status(200).json(users);
+  } catch (err) {
+    console.log("Error", err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-module.exports.getById=async (req,res)=>{
-    try{
-        const user=await Student.find({_id:req.params.id});
-        if(user){
-            return res.send(user);
-        }else{
-            return res.send("Student Not found");
-        }
-    }catch(err){
-        console.log("Error",err);
-        return res.status(500);
+module.exports.getById = async (req, res) => {
+  try {
+    const user = await Student.findById(req.params.id);
+    if (user) {
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).json({ message: "Student not found" });
     }
+  } catch (err) {
+    console.log("Error", err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-module.exports.updateStudent=async(req,res)=>{
-    try{
-        const user=await Student.findByIdAndUpdate(req.params.id,req.body);
-        if(user){
-            return res.send(user);
-        }else{
-            return res.send("Student Not found");
-        }
-    }catch(err){
-        console.log("Error",err);
-        return res.status(500);
+module.exports.updateStudent = async (req, res) => {
+  try {
+    const user = await Student.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (user) {
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).json({ message: "Student not found" });
     }
+  } catch (err) {
+    console.log("Error", err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-module.exports.deleteStudent=async(req,res)=>{
-    try{
-        const user=await Student.findByIdAndDelete(req.params.id);
-        if(user){
-            return res.send(user);
-        }else{
-            return res.send("Student Not found");
-        }
-    }catch(err){
-        console.log("Error",err);
-        return res.status(500);
+module.exports.deleteStudent = async (req, res) => {
+  try {
+    const user = await Student.findByIdAndDelete(req.params.id);
+    if (user) {
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).json({ message: "Student not found" });
     }
+  } catch (err) {
+    console.log("Error", err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
